ffa29e0a8eac9f6ff990abc239045fd4ee21a499
KC Barrett <kbarre123@gmail.com>
2014-01-27 19:47:16 -0600

    Finished FaceViewer.

---
+++ homework/ch02/section_10/FaceViewer/src/faceviewer/FaceViewer.java
@@ -0,0 +1,26 @@
+/*
+ * Draw the alien face created in FaceComponent in a window.
+ */
+
+package faceviewer;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author kcb
+ */
+public class FaceViewer 
+{
+    public static void main(String[] args) 
+    {
+        JFrame frame = new JFrame();
+        frame.setSize(150, 250);
+        frame.setTitle("An Alien Face");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        FaceComponent component = new FaceComponent();
+        frame.add(component);
+        
+        frame.setVisible(true);
+    }
+}
