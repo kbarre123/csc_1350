e726101f17d0ecf5afdbd6e01af3879da71717a8
kbarre123 <kbarre123@gmail.com>
2014-02-03 10:49:19 -0600

    Begin work on Project #1.

---
+++ projects/project_01/EllipseTester/src/ellipsetester/EllipseTester.java
@@ -0,0 +1,24 @@
+/*
+ * Project 1.
+ */
+package ellipsetester;
+
+/**
+ * @file EllipseTester.java
+ * @author Kenneth C. Barrett
+ * @date 2014-02-03;
+ * Description: TODO-------------
+ * Course: CS1350.01
+ * PAWS ID: kbarr17
+ * Project #: 1
+ * Instructor: Dr. Duncan
+ */
+public class EllipseTester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
