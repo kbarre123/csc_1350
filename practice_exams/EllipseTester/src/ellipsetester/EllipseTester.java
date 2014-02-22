a33da7aa049795e01ddb31bd09da7399ae588011
KC Barrett <kbarre123@gmail.com>
2014-02-22 11:43:53 -0600

    Updates

---
+++ practice_exams/EllipseTester/src/ellipsetester/EllipseTester.java
@@ -0,0 +1,16 @@
+package ellipsetester;
+
+/**
+ *
+ * @author kcb
+ */
+public class EllipseTester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
