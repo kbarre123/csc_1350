4de895a463d7bc5dd25624823a17aa61f78ef956
KC Barrett <kbarre123@gmail.com>
2014-02-27 11:19:42 -0600

    Finished E3_12

--- homework/ch03/E3_11/SimpleLetterTester/src/simplelettertester/SimpleLetterTester.java
+++ homework/ch03/E3_11/SimpleLetterTester/src/simplelettertester/SimpleLetterTester.java
@@ -15,10 +15,10 @@ public class SimpleLetterTester {
         SimpleLetter letter = new SimpleLetter("Sally", "Billy");
         
         // Add some lines
-        letter.addLine("Dear " + letter.getTo());
+        letter.addLine("Dear " + letter.getTo() + ":");
         letter.addLine("");
         letter.addLine("I am sorry we must part.");
-        letter.addLine("I wish you all the best");
+        letter.addLine("I wish you all the best.");
         letter.addLine("");
         letter.addLine("Sincerly,");
         letter.addLine(letter.getFrom());
