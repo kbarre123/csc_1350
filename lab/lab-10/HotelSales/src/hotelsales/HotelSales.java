91231d878b506561a270f8b5b365bbf01cd6a9f9
KC Barrett <kbarre123@gmail.com>
2014-04-28 13:49:37 -0500

    Lab-11, assignment b.

---
+++ lab/lab-10/HotelSales/src/hotelsales/HotelSales.java
@@ -0,0 +1,18 @@
+/**
+ * @author kbarr17
+ * Purpose: Sum sales by service and export to separate files.
+ */
+
+package hotelsales;
+
+public class HotelSales 
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
